051b2e45218c7db6a1a81a82ffdea02b5d001df8
MorB <576744651@qq.com>
2019-03-22 15:35:28 +0800

    1.0

---
+++ interface-core/src/main/java/com/lier/core/domain/AppUser.java
@@ -0,0 +1,13 @@
+package com.lier.core.domain;
+
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Table(name = "dgg_user_app")
+public class AppUser implements Serializable {
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
